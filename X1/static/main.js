4a695b0211f00fe562dfda658d7e83b8949c704f
Brian Liu <bliu000@lmbhn3330.bhnetwork.local>
2015-08-13 16:46:12 -0700

    X1 mockups

---
+++ X1/static/main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+
+// if ((navigator.userAgent.match(/iPhone/)) || (navigator.userAgent.match(/iPod/))) {
+//   $('#hint').hide();  
+//   $(".info_small:hover").css("opacity", "1");
+// }
+
+// if (navigator.userAgent.match(/Android/)) {
+//   $('#hint').hide();
+//   $(".info_small:hover").css("opacity", "1"); 
+// }
+
+// window.onresize = function(event) {
+//  	$('#hint').delay( 800 ).fadeOut(),100;
+// };
+
+});
